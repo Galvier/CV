49917254250ee0aa40ccbe9d5b19e6919b08cbf7
Galvier <jonathangalvier@gmail.com>
2022-07-15 19:42:22 -0300

    English page

--- translate.js
+++ translate.js
@@ -1,7 +1,6 @@
 $(function() {
     $(".br").click(function(){
         //Cabeçalho
-        // $(".menu").children().eq(0).text("Início");
         $(".menu").children().eq(0).children().text("Início");
         $(".menu").children().eq(1).children().text("Sobre");
         $(".menu").children().eq(2).children().text("Skills");
@@ -9,7 +8,12 @@ $(function() {
         $(".menu").children().eq(4).children().text("Portfólio");
         $(".menu").children().eq(5).children().text("Contato");
         $(".language-selected").text("PT-BR");
-        $(".language-selected").removeClass('change-en').addClass("change-br");
+        $(".language-selected").removeClass('change-en').addClass("change-br");        
+        $(".about .title").removeClass("change-en");
+        $(".skills .title").removeClass("change-en");
+        $(".qualification .title").removeClass("change-en");
+        $(".portfolio .title").removeClass("change-en");
+        $(".contact .title").removeClass("change-en");
         //Seção
         $(".text-1").text("Olá, bem vindo ao meu site!");
         $(".text-3").text("").append("<span>" + "Desenvolvedor" +'</span>'+ ' web');
@@ -18,9 +22,9 @@ $(function() {
         $(".about .text").text("").append("Olá, me chamo Jonathan e sou" + "<span>" + " Desenvolvedor" +'</span>'+ ' web');
         $(".typing-2").text("Desenvolvedor");
         $(".about .right").children().eq(1).text("Atualmente estou em transição para a carreira de DEV, mas sempre tive contato como fuçador de tecnologia desde o Windows 98. Finalmente consegui um tempo para me especializar e dedicar e aqui estou aprendendo a ser um desenvolvedor completo! Esse é meu primeiro grande projeto que consegui consolidar as principais linguagens que aprendi até agora. Espero que goste do meu humilde portifolio.");
-        $(".about .right").children().eq(2).text("Considere isso uma pequena introdução breve sobre mim e dos meus projetos, você pode conferir um pouco mais clicando no meu currículo completo abaixo, ou veja um pouco mais na aba “Qualificações”!");
-        $(".about .right a:nth-child(1)").text("Currículo");
-        $(".right a:nth-child(2)").text("Certificados");
+        $(".about .right").children().eq(3).text("Considere isso uma pequena introdução breve sobre mim e dos meus projetos, você pode conferir um pouco mais clicando no meu currículo completo abaixo, ou veja um pouco mais na aba “Qualificações”!");
+        $(".about .right a").text("Currículo");
+        $(".about .right a").eq(1).text("Certificados");
         //Skills
         $(".skills .title").text("Skills");
         $(".skills .text").text("Minhas habilidades");
@@ -41,17 +45,18 @@ $(function() {
         $('#edu2_area_2').text('Gestão e Governança TI')
         $('#edu2_year_2').text("").append("<i class='far fa-calendar-alt'>" + "</i>" + " Mar 2022");
         $('#edu2_area_3').text('Arquiteto de Soluções')
+        $('#mattersXP').text("").append("<i class='fas fa-book'>" + "</i>" + " XP Educação");
         $('#edu2_year_3').text("").append("<i class='far fa-calendar-alt'>" + "</i>" + " Out 2021");
         //Portfólio
         $(".portfolio .title").text("Meu portfólio");
-        $('portfolio-space .info-type').text("").append("<i class='fa-solid fa-gears'>" + "</i>" + " Trabalhando");
-        $('portfolio-space .info-data').text("Em breve..");
+        $('.portfolio-space .info-type').text("").append("<i class='fa-solid fa-gears'>" + "</i>" + " Trabalhando");
+        $('.portfolio-space .info-data').text("Em breve..");
         //Contato
         $(".contact .title").text("Contato");
         //seção 1
         $(".contact .left .text").text("Quer conversar comigo?");
-        $(".contact .left .text").children().text("!Se quiser conversar ou discutir algo pode mandar um e-mail ao lado ou ir nas minhas redes sociais abaixo.");
-        $("#nomeCont").text("Nomell");
+        $(".contact .left p").children().text("Se quiser conversar ou discutir algo pode mandar um e-mail ao lado ou ir nas minhas redes sociais abaixo.");
+        $("#nomeCont").text("Nome");
         $("#endCont").text("Endereço");
         $("#emailCont").text("E-mail");
         //seção 2
@@ -79,24 +84,68 @@ $(function() {
         $(".menu").children().eq(5).children().text("Contact");
         $(".language-selected").text("EN-US");
         $(".language-selected").removeClass("change-br").addClass("change-en");
+        $(".about .title").addClass("change-en");
+        $(".skills .title").addClass("change-en");
+        $(".qualification .title").addClass("change-en");
+        $(".portfolio .title").addClass("change-en");
+        $(".contact .title").addClass("change-en");
         //Seção
         $(".text-1").text("Hello, welcome to my website!");
-        // $("span#developer.dev").text(val);
-        // console.log($("span.dev").text());
         $(".text-3").text("Web ");
         $(".text-3").append("<span>" + "Developer" +'</span>');
         //Sobre 
-        $(".about .title").text("Sobre mim");
-        $(".about .text").text("Olá, me chamo Jonathan e sou web");
-        $(".typing-2").text("Desenvolvedor");
-        $(".about .right p").text("Atualmente estou em transição para a carreira de DEV, mas sempre tive contato como fuçador de tecnologia desde o Windows 98. Finalmente consegui um tempo para me especializar e dedicar e aqui estou aprendendo a ser um desenvolvedor completo! Esse é meu primeiro grande projeto que consegui consolidar as principais linguagens que aprendi até agora. Espero que goste do meu humilde portifolio.");
-        $(".about .right p").text("Considere isso uma pequena introdução breve sobre mim e dos meus projetos, você pode conferir um pouco mais clicando no meu currículo completo abaixo, ou veja um pouco mais na aba “Qualificações”!");
-        $(".about .right a:nth-child(1)").text("Currículo");
-        $(".right a:nth-child(2)").text("Certificados");
+        $(".about .title").text("About me");
+        $(".about .text").text("").append("Hi, my name is Jonathan and I'm a web" + "<span>" + " Developer" +'</span>');
+        $(".typing-2").text("Developer");
+        $(".about .right").children().eq(1).text("I'm currently transitioning to a DEV career, but I've always been in touch with technology since Windows 98. I finally got some time to specialize and dedicate myself and here I am, learning to be a complete developer! This is my first big project that I managed to consolidate the main languages I've learned so far. I hope you like my humble portfolio.");
+        $(".about .right").children().eq(3).text("Consider this a short introduction to me and my projects, you can check out a little more by clicking on my full resume below, or see a little more under the “Qualifications” tab!");
+        $(".about .right a").text("Curriculum");
+        $(".about .right a").eq(1).text("Certificates");
         //Skills
         $(".skills .title").text("Skills");
-        $(".skills .text").text("Minhas habilidades");
-        console.log(document.body.childNodes);
+        $(".skills .text").text("My skills");
+        $(".skills .left").children().eq(1).text("I have lined up here, in descending order, the languages and technologies that I have the most experience with. I'm fascinated by the immediate result, so I'm expected to be more adept at the web and related languages (like JavaScript). In addition to great power, these technologies allowed me to measure my learning more quickly, in addition to fostering more creativity and freedom in projects.");
+        // console.log(document.body.childNodes);
+        //Qualificações
+        $(".qualification .title").text("Experience and education");
+            //seção 1
+        $(".education-1 .qualification__title").text("").append("<i class='fas fa-graduation-cap'>" + "</i>" + " Education");
+        $('#edu1_area_1').text('Graduate in Science and Technology')
+        $('#edu1_year_1').text("").append("<i class='far fa-calendar-alt'>" + "</i>" + " Dec 2017 - Complete");
+        $('#edu1_area_2').text('Front End Developer')
+        $('#edu1_year_2').text("").append("<i class='far fa-calendar-alt'>" + "</i>" + " Jun 2022 - At the moment");
+            //seção 2
+        $(".education-2 .qualification__title").text("").append("<i class='fas fa-rocket'>" + "</i>" + " Certificates");
+        $('#edu2_area_1').text('JavaScript')
+        $('#edu2_year_1').text("").append("<i class='far fa-calendar-alt'>" + "</i>" + " Apr 2022");
+        $('#edu2_area_2').text('IT Management and Governance')
+        $('#edu2_year_2').text("").append("<i class='far fa-calendar-alt'>" + "</i>" + " Mar 2022");
+        $('#edu2_area_3').text('Solutions architect')
+        $('#mattersXP').text("").append("<i class='fas fa-book'>" + "</i>" + " XP Education");
+        $('#edu2_year_3').text("").append("<i class='far fa-calendar-alt'>" + "</i>" + " Oct 2021");
+        //Portfólio
+        $(".portfolio .title").text("Meu portfólio");
+        $('.portfolio-space .info-type').text("").append("<i class='fa-solid fa-gears'>" + "</i>" + " Working");
+        $('.portfolio-space .info-data').text("Coming soon..");
+        //Contato
+        $(".contact .title").text("Contato");
+        //seção 1
+        $(".contact .left .text").text("Do you want to talk with me?");
+        $(".contact .left p").text("If you want to talk or discuss something you can send an email to the side or go to my social networks below.");
+        $("#nomeCont").text("Name");
+        $("#endCont").text("Address");
+        $("#emailCont").text("E-mail");
+        //seção 2
+        $(".contact .right .text").text("Send me a message");
+        //form
+        $(".contact .right .name").text("").append("<input name='name' type='text' placeholder='Name' required>")
+        $(".contact .right .email").text("").append("<input name='email' type='text' placeholder='Your e-mail' required>")
+        $(".contact .right .assunto").text("").append("<input name='subject' type='text' placeholder='Subject' required>")
+        $(".contact .right .textarea").text("").append("<textarea name='message' cols='30' rows='10' placeholder='Message..' required></textarea>")
+        $(".contact .right .button-area").text("").append("<button type='submit'>Send</button>");
+        //footer
+        $("#footer").text("").append('<span>| Created by <a href="https://github.com/galvier" target="_blank">Jonathan Galvier</a> | <a href="https://github.com/galvier" target="blank"><i class="fab fa-github"></i></a> | <a href="https://www.linkedin.com/in/jonathangalvier" target="blank"><i class="fab fa-linkedin"></i></a>');
+
     });
 });
 
